9c8ddd6428da9576daca78b4b4537d21558ee4c4
HongBunpheng <hongbunpheng89@gmail.com>
2025-06-04 01:59:56 +0700

    Homepage

---
+++ lib/data/mock_data.dart
@@ -0,0 +1,29 @@
+const destinations = [
+  'assets/images/show-1.jpg',
+  'assets/images/show-2.jpg',
+  'assets/images/show-5.jpg',
+];
+
+const provinces = [
+  {'name': 'KOH RONG', 'img': 'assets/images/province-1.png'},
+  {'name': 'PREAH VIHEAR', 'img': 'assets/images/province-2.png'},
+  {'name': 'PHNOM PENH', 'img': 'assets/images/province-3.png'},
+  {'name': 'KOMPOT', 'img': 'assets/images/province-3.png'},
+];
+
+const events = [
+  {
+    'title': 'Water Festival',
+    'date': '4 Nov - 6 Nov',
+    'location': 'Phnom Penh, Cambodia',
+    'desc':
+        'The Water Festival 2025 in Cambodia celebrates Tonle Sap River’s flow reversal. Boat races, fireworks, and more.',
+    'img': 'assets/images/event-1.jpg',
+  },
+];
+
+const topProvinces = [
+  {'name': 'Phnom Penh', 'img': 'assets/images/top-1.jpg'},
+  {'name': 'Mondukiri', 'img': 'assets/images/top-2.jpg'},
+  {'name': 'Kep', 'img': 'assets/images/top-3.jpg'},
+];
